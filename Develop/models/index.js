f26a279b9c546e4dbd367e0bb6be7252a693959f
dustin.knowles <dustin.knowles22@gmail.com>
2021-03-14 12:59:28 -0700

    creating models and setting up relationships between models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,15 +3,31 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Category');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
+  Category.hasMany(Product, {
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE'
+  });
 
 // Products belongToMany Tags (through ProductTag)
-
+  Product.belongsToMany(Tag, {
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE'
+  });
 // Tags belongToMany Products (through ProductTag)
+  Tag.belongsToMany(Product, {
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE'
+  });
 
+  
 module.exports = {
   Product,
   Category,
